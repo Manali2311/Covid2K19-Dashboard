f40e95ac9966657592ff338f4288b8677b91e5c8
manalli <manallisharma2311@gmail.com>
2020-09-21 15:42:38 +0530

    Updates

---
+++ precache-manifest.2754a42133060eed9bb24e58d12ed8ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66b04df0762ee90506a7b009f92c90f",
+    "url": "/Covid2K19-Dashboard/index.html"
+  },
+  {
+    "revision": "b96453d3e95beaf3ce5d",
+    "url": "/Covid2K19-Dashboard/static/css/main.77b54fea.chunk.css"
+  },
+  {
+    "revision": "7a05606e73ed2cd8b711",
+    "url": "/Covid2K19-Dashboard/static/js/2.12814948.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid2K19-Dashboard/static/js/2.12814948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96453d3e95beaf3ce5d",
+    "url": "/Covid2K19-Dashboard/static/js/main.002dcd9f.chunk.js"
+  },
+  {
+    "revision": "76f9063d3e0abd44d7b0",
+    "url": "/Covid2K19-Dashboard/static/js/runtime-main.f23d5399.js"
+  }
+]);
